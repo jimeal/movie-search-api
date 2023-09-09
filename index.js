8ee4b301ff36d17d9da632ea1696acc0afbd868e
Bae Ji-min <dncl46@gmail.com>
2023-09-09 15:55:20 +0900

    주소창 없애기 삭제

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import $ from "jquery";
 
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.style.height = (document.documentElement.clientHeight) + 'px';
-  setTimeout(scrollTo, 0, 0, 1);
-}, false);
 
 function movieRank() {
   const targetDate = $("#targetDateInput").val();
